3e04e465af97db2367171aee830b22666725b427
DroidSaurav <131275580+DroidSaurav@users.noreply.github.com>
2023-09-15 15:47:42 +0530

    Initial Commit

--- CF/models.py
+++ CF/models.py
@@ -8,4 +8,11 @@ class forum(models.Model):
     link = models.CharField(max_length=100, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     
+    def __str__(self):
+        return str(self.topic)
     
+class Discussion(models.Model):
+    forum = models.ForeignKey(forum, blank=True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.forum)
